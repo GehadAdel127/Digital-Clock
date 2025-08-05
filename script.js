63f329380271995c1f259c3ea152920fcf11f549
gehad <gehadadel6111@gmail.com>
2025-08-05 22:56:20 +0300

    update

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function updateClock(){
     let hour = timeNow.getHours().toString().padStart(2 , 0)
     const meridiem = hour > 12 ? "PM" : "AM"
     hour = hour % 12 || 12
-    const minute = timenow.getMinutes().toString().padStart(2 , 0)
-    const second = timenow.getSeconds().toString().padStart(2 , 0)
+    const minute = timeNow.getMinutes().toString().padStart(2 , 0)
+    const second = timeNow.getSeconds().toString().padStart(2 , 0)
     const timeInString = `${hour}:${minute}:${second}:${meridiem}`
     document.getElementById("clock").textContent = timeInString
 }
