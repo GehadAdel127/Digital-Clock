e6a9cabef84fbb579b047a8a0fc843a12b92144b
gehad <gehadadel6111@gmail.com>
2025-08-05 22:55:37 +0300

    solve an error

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function updateClock(){
-    const timenow = new Date()
-    let hour = timenow.getHours().toString().padStart(2 , 0)
+    const timeNow = new Date()
+    let hour = timeNow.getHours().toString().padStart(2 , 0)
     const meridiem = hour > 12 ? "PM" : "AM"
     hour = hour % 12 || 12
     const minute = timenow.getMinutes().toString().padStart(2 , 0)
